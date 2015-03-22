b851f9344f79cd96a4721b55e31952aa82346d27
rando345 <randotonisson@gmail.com>
2015-03-22 20:37:49 +0200

    veel jama

--- lisakandidaat.php
+++ lisakandidaat.php
@@ -1,23 +1,3 @@
-
-<p>Täida ankeet ja kliki <strong>salvesta</strong> .</p>
-<form method="post" action="lisakandidaat.php" enctype="multipart/form-data" >
-	Valimised: 
-		<input type="radio" name="election" value="riigikogu" checked>
-		<label for="e1">Riigikogu valimised</label><br />
-		<input type="radio" name="election" value="kohalik">
-		<label for="e2">Kohaliku volikogu valimised</label><br />
-		<input type="radio" name="election" value="president">
-		<label for="e3">Presidendi valimised</label><br />
-	Name <input type="text" name="name" id="name"/></br>
-	Info <input type="text" name="info" id="info"/></br>
-	Page <input type="text" name="page" id="page"/></br>
-	Anna ka hääl?:
-		<input type="radio" name="votes" value="0" checked>
-		<label for="e1">Ei</label><br />
-		<input type="radio" name="votes" value="1">
-		<label for="e2">Jah</label><br />
-	<input type="submit" name="submit" value="Salvesta" />
-</form>
 <?php
 // DB connection info
 $host = "eu-cdbr-azure-north-c.cloudapp.net";
@@ -79,6 +59,7 @@ if(count($elections) > 0) {
         echo "<td>".$election['date']."</td></tr>";
     }
     echo "</table>";
+	mysql_close();
 } else {
     echo "<h3>No one is currently registered.</h3>";
 }
