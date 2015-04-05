b44e47f58a8c2cefc5425ec3e73464674b800254
rando345 <randotonisson@gmail.com>
2015-04-05 15:22:36 +0300

    uus sql parandus

--- topriigikogu.php
+++ topriigikogu.php
@@ -13,7 +13,7 @@ catch(Exception $e){
     die(var_dump($e));
 }
 
-$sql_select = "SELECT TOP 10 * FROM elections_tbl where election='riigikogu' Order by votes DESC";
+$sql_select = "SELECT TOP 10 Name, Votes FROM elections_tbl where election='riigikogu' Order by votes DESC";
 $stmt = $conn->query($sql_select);
 $elections = $stmt->fetchAll(); 
 if(count($elections) > 0) {
