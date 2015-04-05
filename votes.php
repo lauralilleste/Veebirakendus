b57d3fad34a4412f613289027868041f10fe4e6d
rando345 <randotonisson@gmail.com>
2015-04-05 18:50:33 +0300

    debug5

--- votes.php
+++ votes.php
@@ -19,8 +19,7 @@ $sql_select = "SELECT SUM(Votes) AS value_1 FROM elections_tbl";
 $stmt = $conn->query($sql_select);
 $elections = $stmt->fetchAll(); 
 if(count($elections) > 0) {
-	print_r($elections);
-	$sum = $elections;
+	$sum = $elections[0][value_1];
     echo "kokku on hääletatud: ";
     echo $sum;
 } else {
