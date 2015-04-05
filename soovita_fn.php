ae41480d4beede9ea21f9aa7fe16861b691f6364
rando345 <randotonisson@gmail.com>
2015-04-05 19:47:38 +0300

    uus

--- soovita_fn.php
+++ soovita_fn.php
@@ -70,6 +70,5 @@ session_start();
 }
 else {
 	echo "Selle lehe vaatamiseks pead olema sisse logitud! Logi sisse vajutades Facebook logole.";
-	echo '<b href='.$helper->getLoginUrl().'><img style="width: 250px; height: 75px;"  border="0" src="Facebooklogo.jpeg"></b>';
 }
 ?>
