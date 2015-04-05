5ab9905c4f6fbca49bd76f88b140bf2f20a6f2d2
rando345 <randotonisson@gmail.com>
2015-04-05 14:57:09 +0300

    .

--- index.php
+++ index.php
@@ -5,38 +5,7 @@ include "header.php";
 		<div class="screen">
 			<div class="top">
 				<p>Top 10 riigikogu kandidaati.</p>
-				<?php
-					// DB connection info
-					$host = "eu-cdbr-azure-north-c.cloudapp.net";
-					$user = "b17689f0b6725e";
-					$pwd = "ce8a018e";
-					$db = "soovitaDB";
-					// Connect to database.
-					try {
-						$conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
-						$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-					}
-					catch(Exception $e){
-						die(var_dump($e));
-					}
-
-					$sql_select = "SELECT * FROM elections_tbl Order by votes  DESC LIMIT 10 where election='riigikogu'";
-					$stmt = $conn->query($sql_select);
-					$elections = $stmt->fetchAll(); 
-					if(count($elections) > 0) {
-						echo "<table>";
-						echo "<tr><th>Name</th>";
-						echo "<th>Votes</th>";
-						foreach($elections as $election) {
-							echo "<td>".$election['name']."</td>";
-							echo "<td>".$election['votes']."</td>""</tr>";
-						}
-						echo "</table>";
-						mysql_close();
-					} else {
-						echo "<h3>Kedagi pole lisatud.</h3>";
-					}
-				?>
+				<?php include 'topriigikogu.php';?>
 			</div>
 			<div class="top">
 				<p>Top 10 kohalike omavalitsuste kandidaati.</p>
