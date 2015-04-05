900a26df39f005494872b2e393d9c83c4148210a
rando345 <randotonisson@gmail.com>
2015-04-05 13:25:36 +0300

    test

---
+++ index1.php
@@ -0,0 +1,18 @@
+<?php include "header.php"; ?>
+		<div class="screen">
+			<div class="top">
+				<p>Top 10 riigikogu kandidaati.</p>
+
+
+			</div>
+			<div class="top">
+				<p>Top 10 kohalike omavalitsuste kandidaati.</p>
+				<?php include 'topkohalik.php';?>
+			</div>
+			<div class="top">
+				<p>Top 10 presidendi kanditaati.</p>
+				<?php include 'toppresident.php';?>
+			</div>
+		</div>
+		
+<?php include "footer.php";>
