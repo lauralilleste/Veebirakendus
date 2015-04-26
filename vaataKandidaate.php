16e4ba7b171fd2afdf316fabf7b3bb9f04e8e101
lauralilleste <lauralilleste@gmail.com>
2015-04-26 23:37:08 +0300

    kandidaatide järjestus

--- vaataKandidaate.php
+++ vaataKandidaate.php
@@ -13,7 +13,7 @@ catch(Exception $e){
     die(var_dump($e));
 }
 
-$sql_select = "SELECT * FROM elections_tbl";
+$sql_select = "SELECT * FROM elections_tbl Order by votes DESC";
 $stmt = $conn->query($sql_select);
 $elections = $stmt->fetchAll(); 
 if(count($elections) > 0) {
